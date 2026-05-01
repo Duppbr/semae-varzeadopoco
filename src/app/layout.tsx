1e9b9857eeb5749d460ae05747f1666efcb58209
Duppbr <duppbr10@gmail.com>
2026-05-01 15:36:53 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,12 @@ import Navbar from '@/components/Navbar';
 export const metadata: Metadata = {
   title: 'Rios Baterias - Consulta de Preços',
   description: 'Sistema interno de consulta de baterias',
+  manifest: '/manifest.json', // <-- indica o manifesto
+  icons: {
+    icon: '/icon-192.png',    // ícone padrão
+    shortcut: '/icon-192.png',
+    apple: '/icon-192.png',
+  },
 };
 
 export default function RootLayout({
